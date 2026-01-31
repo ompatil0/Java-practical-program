dd22b0105547fdcf66db38622319e1e2a188a042
Om Patil <patilomh@gmail.com>
2026-01-31 19:06:41 +0530

    Java practical programs added

---
+++ ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
